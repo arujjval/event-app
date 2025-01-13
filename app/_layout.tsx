bc3d810bd7fe9cace7e0404761d11d183cb8b06f
Arujjwal <arujjwal73@gmail.com>
2025-01-13 17:34:13 +0530

    set up of icons, nativeWind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,35 +5,22 @@ import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import './global.css'
+import SignIn from './signIn';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
-
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
+  if(true) {
+    return (
+        <SignIn />
+    )
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
+    <ThemeProvider value={ DefaultTheme }>
+      <Stack screenOptions={{ headerShown: false }}/>
     </ThemeProvider>
   );
 }
---
+++ constants/icons/index.tsx
@@ -0,0 +1,16 @@
+export { default as AlarmClock } from './alarm-clock.png';
+export { default as AngleLeft } from './angle-left.png';
+export { default as Calendar } from './calendar.png';
+export { default as Google } from './google.png';
+export { default as Heart } from './heart.png';
+export { default as Home } from './home.png';
+export { default as Marker } from './marker.png';
+export { default as Portrait } from './portrait.png';
+export { default as Search } from './search.png';
+export { default as SettingsSliders } from './settings-sliders.png';
+export { default as StarChristmas } from './star-christmas.png';
+export { default as Upload } from './upload.png';
+export { default as UserCircle } from './user_circle.png';
+export { default as User } from './user.png';
+export { default as Users } from './users.png';
+export { default as VideoCameraAlt } from './video-camera-alt.png';
